1d0c98be6b772fa7dddc9801bb0f68e38bf8d90a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-07-16 12:55:19 +0700

    Create a main function and main router

--- cmd/ElectiVote/main.go
+++ cmd/ElectiVote/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func init() {
+
+}
+
+func main() {
+	router := gin.Default()
+	{
+		router.LoadHTMLGlob("/internal/static/html/*.html")
+	}
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
