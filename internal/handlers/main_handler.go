1c6945c3b0e2fae0411670aa597032e19a25db21
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-07-18 14:39:39 +0700

    Create logic for temporary home page

---
+++ internal/handlers/main_handler.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/AndreanDjabbar/CaysAPIHub/internal/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func ViewHomePage(c *gin.Context) {
+	if !middlewares.IsLogged(c) {
+		c.Redirect(
+			http.StatusFound,
+			"/electivote/login-page/",
+		)
+		return
+	}
+
+	dataUser := middlewares.GetUserData(c)
+
+	context := gin.H {
+		"title": "Home",
+		"dataUser": dataUser,
+	}
+	c.HTML(
+		http.StatusOK,
+		"home.html",
+		context,
+	)
+}
