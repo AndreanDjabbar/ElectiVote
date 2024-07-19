2f5e13a7d73496069cd9a5d61054351f57b2ab75
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-07-19 23:42:55 +0700

    Create utility for login and register input validation

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -39,3 +39,66 @@ func HashPassword(password string) (string, error) {
     return string(hashedPassword), nil
 } 
 
+func ValidateLoginInput(username, password string) (string, string) {
+	usernameErr, passwordErr := "", ""
+
+	if username == "" {
+		usernameErr = "Username Must be Filled"
+	}
+
+	if password == "" {
+		passwordErr = "Password Must be Filled"
+	}
+
+	if len(username) != 0 && (len(username) < 5 || len(username) > 255) {
+		usernameErr = "Username must be between 5 and 255 characters"
+	}
+
+	if len(password) != 0 && (len(password) < 5 || len(password) > 255) {
+		passwordErr = "Password must be between 5 and 255 characters"
+	}
+
+	return usernameErr, passwordErr
+}
+
+func ValidateRegisterInput(username, password, password2, email string) (string, string, string, string) {
+	usernameErr, passwordErr, password2Err,  emailErr := "", "", "", ""
+
+	if username == "" {
+		usernameErr = "Username must be filled"
+	}
+
+	if password == "" {
+		passwordErr = "Password must be filled"
+	}
+
+	if password2 == "" {
+		passwordErr = "Password Confirmation must be filled"
+	}
+
+	if password2 != "" && password != password2 {
+		password2Err = "Password and Password Confirmation must be the same"
+	}
+
+	if email == "" {
+		emailErr = "Email must be filled"
+	}
+
+	if len(username) != 0 && (len(username) < 5 || len(username) > 255) {
+		usernameErr = "Username must be between 5 and 255 characters"
+	}
+
+	if len(password) != 0 && (len(password) < 5 || len(password) > 255) {
+		passwordErr = "Password must be between 5 and 255 characters"
+	}
+
+	if len(password2) != 0 && (len(password2) < 5 || len(password2) > 255) {
+		passwordErr = "Password must be between 5 and 255 characters"
+	}
+
+	if email != "" && !IsValidEmail(email) {
+		emailErr = "Email must contain @ and end with .com or .co.id"
+	}
+	
+	return usernameErr, passwordErr, password2Err, emailErr
+}
