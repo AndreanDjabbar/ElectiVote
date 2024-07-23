9c7a3881a671bd805625cdf7fa3d2e49f004f3bf
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-07-23 21:30:16 +0700

    Change database to mysql

--- internal/db/connector.go
+++ internal/db/connector.go
@@ -1,26 +1,26 @@
 package db
 
 import (
-	"path/filepath"
-
 	"github.com/AndreanDjabbar/CaysAPIHub/internal/models"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectToDatabase() {
-	absPath, err := filepath.Abs("../../internal/db/electivote.db")
+
 	database, err := gorm.Open(
-		sqlite.Open("file:" + absPath + "?cache=shared&_loc=auto"),
-		&gorm.Config{},
+		mysql.Open(
+			"ElectiVote:ElectiVote12@tcp(localhost:3306)/ElectiVote",
+		),
 	)
 	if err != nil {
 		panic(err.Error())
 	}
 	database.AutoMigrate(
 		&models.User{},
+		&models.Profile{},
 	)
 	DB = database
 }
