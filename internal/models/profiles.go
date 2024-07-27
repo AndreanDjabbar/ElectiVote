969685a9c43338f0ac7dd12d7974fbfad59b3828
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-07-27 19:55:53 +0700

    Create profiles.go to seperaring model

---
+++ internal/models/profiles.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+type Profile struct {
+	ProfileID   uint     `gorm:"primary_key"`
+	Picture     string   `gorm:"type:varchar(255)"`
+	FirstName   string   `gorm:"type:varchar(255)"`
+	LastName    string   `gorm:"type:varchar(255)"`
+	Age         uint     `gorm:"type:int;default:NULL"`
+	PhoneNumber string   `gorm:"type:varchar(255)"`
+	Birthday    NullTime `gorm:"type:date;default:NULL"`
+	UserID      uint
+	User        User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"`
+}
+
+type NullTime struct {
+	Time  time.Time
+	Valid bool
+}
+
+func (nt *NullTime) Scan(value interface{}) error {
+	if value == nil {
+		nt.Time, nt.Valid = time.Time{}, false
+		return nil
+	}
+	nt.Valid = true
+	switch v := value.(type) {
+	case time.Time:
+		nt.Time = v
+	case []uint8:
+		t, err := time.Parse("2006-01-02", string(v))
+		if err != nil {
+			return err
+		}
+		nt.Time = t
+	default:
+		return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type *NullTime", value)
+	}
+	return nil
+}
+
+func (nt NullTime) Value() (driver.Value, error) {
+	if !nt.Valid {
+		return nil, nil
+	}
+	return nt.Time, nil
+}
