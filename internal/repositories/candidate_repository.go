8c002fa7e0b2e174063ed254731a7f6f52aede0a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-08-04 18:24:46 +0700

    Add function for get candidates data

--- internal/repositories/candidate_repository.go
+++ internal/repositories/candidate_repository.go
@@ -9,3 +9,9 @@ func AddCandidate(newCandidate models.Candidate) (models.Candidate, error) {
 	err := db.DB.Create(&newCandidate).Error
 	return newCandidate, err
 }
+
+func GetCandidatesByVoteID(voteID uint) ([]models.Candidate, error) {
+	var candidates []models.Candidate
+	err := db.DB.Where("vote_id = ?", voteID).Find(&candidates).Error
+	return candidates, err
+}
