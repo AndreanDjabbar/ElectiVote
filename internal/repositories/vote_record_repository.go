5bae3b64f85e9a6929f164f394ae42183d3c89a6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-09-03 19:03:22 +0700

    Add handler in vote record repository

--- internal/repositories/vote_record_repository.go
+++ internal/repositories/vote_record_repository.go
@@ -6,22 +6,46 @@ import (
 )
 
 func CreateVoteRecord(voteRecord models.VoteRecord) (models.VoteRecord, error) {
+	logger.Info(
+		"Vote Record Repository - Creating Vote Record",
+	)
 	err := db.DB.Create(&voteRecord).Error
 	if err != nil {
+		logger.Error(
+			"Vote Record Repository - Error Creating Vote Record",
+			"error", err,
+		)
 		return voteRecord, err
 	}
+	logger.Info(
+		"Vote Record Repository - Vote Record Created",
+	)
 	return voteRecord, nil
 }
 
 func IsVoted(userID uint, voteCode string) (bool) {
+	logger.Info(
+		"Vote Record Repository - Check User Voted",
+	)
 	voteID, err := GetVoteIDByVoteCode(voteCode)
 	if err != nil {
+		logger.Error(
+			"Vote Record Repository - Error Get Vote ID By Vote Code",
+			"error", err,
+		)
 		return false
 	}
 	voteRecord := models.VoteRecord{}
 	err = db.DB.Where("user_id = ? AND vote_id = ?", userID, voteID).First(&voteRecord).Error
 	if err != nil {
+		logger.Error(
+			"Vote Record Repository - Error Get Vote Record",
+			"error", err,
+		)
 		return false
 	}
+	logger.Info(
+		"Vote Record Repository - User Voted",
+	)
 	return true
 }
