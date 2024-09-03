f92d7bcf6bafdc96ed578b9822fb2769c8cc00b5
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-09-03 19:31:49 +0700

    Add logging in session and cookies middlewares

--- internal/middlewares/cookies.go
+++ internal/middlewares/cookies.go
@@ -1,19 +1,28 @@
 package middlewares
 
 import (
+	"log/slog"
 	"os"
 	"time"
 
+	"github.com/AndreanDjabbar/ElectiVote/config"
 	"github.com/AndreanDjabbar/ElectiVote/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
 var cookieKey string = os.Getenv("COOKIE_KEY")
+var logger *slog.Logger = config.SetUpLogger()
 
 func SetCookies(c *gin.Context, username string) {
+	logger.Info(
+		"SetCookies - setting cookies",
+	)
 	token, err := utils.GenerateSecureToken(username)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to generate secure token"})
+		logger.Error(
+			"SetCookies - error generating secure token",
+			"error", err,
+		)
 		return
 	}
 	expiration := time.Now().Add(7 * 24 * time.Hour)
@@ -29,18 +38,32 @@ func SetCookies(c *gin.Context, username string) {
 }
 
 func GetCookies(c *gin.Context) string {
+	logger.Info(
+		"GetCookies - getting cookies",
+	)
 	cookie, err := c.Cookie(cookieKey)
 	if err != nil {
+		logger.Error(
+			"GetCookies - error getting cookies",
+			"error", err,
+		)
 		return ""
 	} 
 	resultCookie, err := utils.ExtractUsername(cookie)
 	if err != nil {
+		logger.Error(
+			"GetCookies - error extracting username from cookies",
+			"error", err,
+		)
 		return ""
 	}
 	return resultCookie
 }
 
 func IsLogged(c *gin.Context) bool {
+	logger.Info(
+		"IsLogged - checking if user is logged in",
+	)
 	if GetSession(c) != "" {
 		return true
 	} else if GetCookies(c) != "" {
@@ -50,6 +73,9 @@ func IsLogged(c *gin.Context) bool {
 }
 
 func DeleteCookie(c *gin.Context) {
+	logger.Info(
+		"DeleteCookie - deleting cookies",
+	)
 	c.SetCookie(
 		cookieKey,
 		"",
@@ -62,6 +88,9 @@ func DeleteCookie(c *gin.Context) {
 }
 
 func GetUserData(c *gin.Context) string {
+	logger.Info(
+		"GetUserData - getting user session or cookies data",
+	)
 	if GetSession(c) != "" {
 		return GetSession(c)
 	} else if GetCookies(c) != "" {
--- internal/middlewares/sessions.go
+++ internal/middlewares/sessions.go
@@ -12,27 +12,42 @@ import (
 var sessionKey string = os.Getenv("SESSION_KEY")
 
 func SetSession(c *gin.Context, value string) {
+	logger.Info(
+		"SetSession - setting session",
+	)
 	session := sessions.Default(c)
 	session.Set(sessionKey, value)
 	session.Save()
 }
 
 func GetSession(c *gin.Context) string {
+	logger.Info(
+		"GetSession - getting session",
+	)
 	session := sessions.Default(c)
 	value := session.Get(sessionKey)
 	if value == nil {
+		logger.Warn(
+			"GetSession - session is empty",
+		)
 		return ""
 	}
 	return value.(string)
 }
 
 func DeleteSession(c *gin.Context) {
+	logger.Info(
+		"DeleteSession - deleting session",
+	)
 	session := sessions.Default(c)
 	session.Delete(sessionKey)
 	session.Save()
 }
 
 func SetRegisterSession(c *gin.Context, username, email, password, otp string) {
+	logger.Info(
+		"SetRegisterSession - setting register session",
+	)
 	session := sessions.Default(c)
 	session.Set("username", username)
 	session.Set("email", email)
@@ -40,11 +55,17 @@ func SetRegisterSession(c *gin.Context, username, email, password, otp string) {
 	session.Set("otp", otp)
 	session.Set("created_at", time.Now().Unix())
 	if err := session.Save(); err != nil {
+		logger.Error(
+			"SetRegisterSession - error saving session",
+		)
 		fmt.Println("Error saving session: ", err)
 	}
 }
 
 func DeleteRegisterSession(c *gin.Context) {
+	logger.Info(
+		"DeleteRegisterSession - deleting register session",
+	)
 	session := sessions.Default(c)
 	session.Delete("username")
 	session.Delete("email")
