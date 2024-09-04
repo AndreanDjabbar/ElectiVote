cf15f4e9b4599da86ad7c609102d8e3b92b3e122
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-09-04 16:30:10 +0700

    Move route configuration into config package

--- cmd/ElectiVote/main.go
+++ cmd/ElectiVote/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/AndreanDjabbar/ElectiVote/config"
 	"github.com/AndreanDjabbar/ElectiVote/internal/db"
 	"github.com/AndreanDjabbar/ElectiVote/internal/routes"
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
 )
 func init() {
 	db.ConnectToDatabase()
@@ -18,31 +15,14 @@ func init() {
 func main() {
 	logger := config.SetUpLogger()
 	logger.Info("Start setting up server")
-	router := gin.Default()
-	router.LoadHTMLGlob("internal/views/html/*.html")
-	router.Static("/images", "internal/assets/images")
-	router.MaxMultipartMemory = 8 << 20
 
-	mainStore := cookie.NewStore([]byte("main"))
-	mainStore.Options(sessions.Options{
-		MaxAge: 0,
-		HttpOnly: true,
-		Path: "/",
-		Secure: false,
-	})
-
-	router.Use(sessions.Sessions("mainSession", mainStore))
-	port := os.Getenv("PORT")
-	if port == "" {
-		logger.Warn("PORT is not set, using default port 8080")
-		port = "8080" 
-	}
-	host := os.Getenv("HOST")
-	if host == "" {
-		logger.Warn("HOST is not set, using default host localhost")
-		host = "localhost"
-	}
+	router := config.SetUpRouter()
+	router.Use(sessions.Sessions("mainSession", config.SetUpSessionStore()))
 	routes.SetUpRoutes(router)
+
+	host := config.GetHost()
+	port := config.GetPort()
+
 	logger.Info("Server is run on", "host", host, "port", port)
 	err := router.Run(fmt.Sprintf("%s:%s", host, port))
 	if err != nil {
--- config/logger.go
+++ config/logger.go
@@ -3,6 +3,10 @@ package config
 import (
 	"log/slog"
 	"os"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
 func SetUpLogger() *slog.Logger {
@@ -21,4 +25,40 @@ func SetUpLogger() *slog.Logger {
 	
 	logger := slog.New(handler)
 	return logger
+}
+
+func GetHost() string {
+	host := os.Getenv("HOST")
+	if host == "" {
+		return "localhost"
+	}
+	return host
+}
+
+func GetPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+	return port
+}
+
+func SetUpSessionStore() sessions.Store {
+	mainStore := cookie.NewStore([]byte("main"))
+	mainStore.Options(sessions.Options{
+		MaxAge:   0,
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+	})
+	return mainStore
+}
+
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("internal/views/html/*.html")
+	router.Static("/images", "internal/assets/images")
+	router.MaxMultipartMemory = 8 << 20
+	return router
 }
