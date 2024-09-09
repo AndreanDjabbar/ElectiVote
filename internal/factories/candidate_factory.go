60e7d391d0d818229976314b446652970e7dbb48
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-09-09 20:15:09 +0700

    Update logging in factories

--- internal/factories/candidate_factory.go
+++ internal/factories/candidate_factory.go
@@ -1,15 +1,10 @@
 package factories
 
 import (
-	"github.com/AndreanDjabbar/ElectiVote/config"
 	"github.com/AndreanDjabbar/ElectiVote/internal/models"
 )
 
 func CandidateFactory(candidateName, candidateDescription, candidatePicture string, voteID uint) models.Candidate {
-	logger := config.SetUpLogger()
-	logger.Info(
-		"Candidate Factory - Creating Candidate",
-	)
 	return models.Candidate{
 		CandidateName: candidateName,
 		VoteId:        voteID,
