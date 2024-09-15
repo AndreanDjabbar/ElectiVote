23486dee2ae21ee4bfc0e966bd9cbc193c8372e6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-09-15 15:42:28 +0700

    Create handler for about us page

--- internal/handlers/main_handler.go
+++ internal/handlers/main_handler.go
@@ -2,8 +2,9 @@ package handlers
 
 import (
 	"net/http"
-	"github.com/gin-gonic/gin"
+
 	"github.com/AndreanDjabbar/ElectiVote/internal/middlewares"
+	"github.com/gin-gonic/gin"
 )
 
 func ViewHomePage(c *gin.Context) {
@@ -33,6 +34,35 @@ func ViewHomePage(c *gin.Context) {
 	c.HTML(
 		http.StatusOK,
 		"home.html",
+		context,
+	)
+}
+
+func ViewAboutUsPage(c *gin.Context) {
+	if !middlewares.IsLogged(c) {
+		logger.Warn(
+			"ViewHomePage - User is not logged in",
+			"Client IP", c.ClientIP(),
+			"action", "redirecting to login page",
+		)
+		c.Redirect(
+			http.StatusFound,
+			"/electivote/login-page/",
+		)
+		return
+	}
+	username := middlewares.GetUserData(c)
+	logger.Info(
+		"ViewAboutUsPage - rendering about us page",
+		"Client IP", c.ClientIP(),
+		"Username", username,
+	)
+	context := gin.H {
+		"title": "About Us",
+	}
+	c.HTML(
+		http.StatusOK,
+		"aboutUs.html",
 		context,
 	)
 }
