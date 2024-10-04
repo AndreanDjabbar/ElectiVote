64c0c6c44e603f40595f5cdfd85ef29dc25a5b08
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-10-04 15:41:50 +0700

    Update docker compose

--- config/logger.go
+++ config/logger.go
@@ -1,37 +1,24 @@
 package config
 
 import (
-    "log/slog"
-    "os"
-    "time"
+	"log/slog"
+	"os"
 )
 
 func SetUpLogger() *slog.Logger {
-    file, err := os.OpenFile(
-        "logs/ElectiVote.log",
-        os.O_CREATE|os.O_WRONLY|os.O_APPEND,
-        0666,
-    )
-    if err != nil {
-        panic(err)
-    }
+	file, err := os.OpenFile(
+		"logs/ElectiVote.log",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0666,
+	)
+	if err != nil {
+		panic(err) 
+	}
 
-    // Load Asia/Jakarta timezone
-    location, err := time.LoadLocation("Asia/Jakarta")
-    if err != nil {
-        panic(err)
-    }
-
-    // Custom time format function
-    timeFormat := func(t time.Time) string {
-        return t.In(location).Format("2006-01-02 15:04:05")
-    }
-
-    handler := slog.NewTextHandler(file, &slog.HandlerOptions{
-        Level: slog.LevelInfo,
-        TimeFormat: timeFormat,
-    })
-
-    logger := slog.New(handler)
-    return logger
+	handler := slog.NewTextHandler(file, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	
+	logger := slog.New(handler)
+	return logger
 }
